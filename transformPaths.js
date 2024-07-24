9ed28165ca4ef942092437af3fb901873f2c12a4
Ignacio Biran <nqchossj@gmail.com>
2024-07-24 02:35:42 -0300

    add transform paths file & command

---
+++ transformPaths.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+function main() {
+  const path = "./dist/index.html";
+  try {
+    const data = fs.readFileSync(path, { encoding: "utf-8" });
+    let final = data.split('src="./').join('src="');
+    final = final.split('href="./').join('href="');
+    fs.writeFileSync(path, final);
+  } catch (err) {
+    //
+  }
+}
+
+main();
