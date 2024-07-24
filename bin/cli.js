0533f2e73c6307e2ca7d299d996448303e63bfb8
Ignacio Biran <nqchossj@gmail.com>
2024-07-24 02:28:28 -0300

    update cli

--- bin/cli.js
+++ bin/cli.js
@@ -18,15 +18,15 @@ const gitCheckoutCommand = `git clone --depth 1 https://github.com/NBN7/avt-ext 
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(
-  chalk.blue(`Cloning extensions template with name: ${repoName}...\n`)
+  chalk.blue(`\nCloning extensions template with name: ${repoName}...\n`)
 );
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(chalk.blue(`Installing dependencies for: ${repoName}...\n`));
+console.log(chalk.blue(`\nInstalling dependencies for: ${repoName}...\n`));
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
-console.log(chalk.green("Done! Now run:\n"));
-console.log(chalk.yellow(`cd ${repoName}\n`));
+console.log(chalk.green("\nDone! Now run:\n"));
+console.log(chalk.yellow(`cd ${repoName}`));
 console.log(chalk.yellow(`npm run dev\n`));
