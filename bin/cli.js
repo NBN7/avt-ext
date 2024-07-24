fa0aa5c16411d60334bb26fd63f941904007a473
Ignacio Biran <nqchossj@gmail.com>
2024-07-24 02:16:47 -0300

    console colors

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { execSync } from "child_process";
+import chalk from "chalk";
 
 const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: "inherit" });
   } catch (e) {
-    console.error(`Failed to execute ${command}`, e);
+    console.error(chalk.red(`Failed to execute ${command}`), e);
     return false;
   }
   return true;
@@ -17,14 +17,15 @@ const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/NBN7/avt-ext ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-console.log(`Cloning repo ${repoName}...`);
+console.log(chalk.blue(`Cloning repo ${repoName}...`));
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(`Installing dependencies for ${repoName}...`);
+console.log(chalk.blue(`Installing dependencies for ${repoName}...`));
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
-console.log("Congrats! You are ready. Follow the following commands to start");
-console.log(`cd ${repoName} && npm start`);
-/* eslint-enable @typescript-eslint/no-var-requires */
+console.log(
+  chalk.green("Congrats! You are ready. Follow the following commands to start")
+);
+console.log(chalk.yellow(`cd ${repoName} && npm start`));
