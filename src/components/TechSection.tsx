ddc3cc464cf19ceb1f06579d9fe0b5409e348af8
Nacho <ignacio.biran@auravant.com>
2024-07-29 18:05:46 -0300

    add separator || add textarea || add tooltip || update some components || update readme

--- src/components/TechSection.tsx
+++ src/components/TechSection.tsx
@@ -1,8 +1,10 @@
-import ReactLogo from '../assets/react.svg';
-import TypeScriptLogo from '../assets/typescript.svg';
-import TailwindLogo from '../assets/tailwind.svg';
+// remove this file when you start working on the project
 
-import { logoStyle } from '../constants/logoStyle';
+import ReactLogo from '@/assets/react.svg';
+import TypeScriptLogo from '@/assets/typescript.svg';
+import TailwindLogo from '@/assets/tailwind.svg';
+
+const logoStyle = 'size-10 hover:scale-110 transition-all cursor-pointer';
 
 export const TechSection = () => {
   return (
--- src/constants/logoStyle.ts
+++
@@ -1,2 +0,0 @@
-export const logoStyle =
-  'size-10 hover:scale-110 transition-all cursor-pointer';
