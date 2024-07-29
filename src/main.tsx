be7f2992183660683f6280df36b40273874f7b74
Nacho <ignacio.biran@auravant.com>
2024-07-29 12:16:41 -0300

    add sentry and gitlab ci cd

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,39 @@
+import * as Sentry from '@sentry/react';
+
+// enable only if you are using react-router-dom
+// import { useEffect } from 'react';
+// import {
+//   createRoutesFromChildren,
+//   matchRoutes,
+//   useLocation,
+//   useNavigationType,
+// } from "react-router-dom";
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import '@fontsource-variable/open-sans';
+
+Sentry.init({
+  dsn: '',
+  integrations: [
+    // enable only if you are using react-router-dom
+    // Sentry.reactRouterV6BrowserTracingIntegration({
+    //   useEffect,
+    //   useLocation,
+    //   useNavigationType,
+    //   createRoutesFromChildren,
+    //   matchRoutes,
+    // }),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
