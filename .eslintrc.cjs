45c0c4309d0657f4222431ea4f401a134f54efc3
Ignacio Biran <nqchossj@gmail.com>
2024-08-06 16:45:17 -0300

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks', 'react-refresh', '@typescript-eslint'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
