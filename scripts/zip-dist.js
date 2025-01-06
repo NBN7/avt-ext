e7d77d9a94fe907fd1aef68b1d1789b84d91861b
Nacho <ignacio.biran@auravant.com>
2025-01-06 15:11:24 -0300

    remove chalk

--- scripts/zip-dist.js
+++ scripts/zip-dist.js
@@ -1,22 +1,9 @@
 import archiver from 'archiver';
-import chalk from 'chalk';
 import fs from 'fs';
 
 const output = fs.createWriteStream('dist.zip');
 const archive = archiver('zip', {
   zlib: { level: 9 },
-});
-
-output.on('close', () => {
-  console.log(
-    chalk.blue(
-      `Auravant Extension .zip file created successfully: ${archive.pointer()} bytes\n`
-    )
-  );
-});
-
-archive.on('error', (err) => {
-  throw err;
 });
 
 archive.pipe(output);
